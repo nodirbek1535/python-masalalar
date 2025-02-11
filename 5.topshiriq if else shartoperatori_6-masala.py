6a83cb865b2f9e6befa0ab5cb8fbf163a93e8ec7
Nodirbek Nasrullayev <Sulaymonovnasim@gmail.com>
2025-02-11 23:58:57 +0500

    if else shartiga oid masala

---
+++ 5.topshiriq if else shartoperatori_6-masala.py
@@ -0,0 +1,13 @@
+n=int(input("nechta son kiritmoqchisiz:"))
+a=[0]*n
+for i in range(n):
+    a[i]=int(input(f"{i+1}-elementni kiriting: "))
+print(a)
+count=0
+for i in range(n):
+    if(a[i]<0):
+        count+=1
+print(f"massivda {count} ta manfiy son mavjud")
+
+manfiy = list(filter(lambda x: x < 0, [(lambda x: int(input(f"{x+1}-elementni kiriting: ")))(i) for i in range(int(input("nechta son kiritmoqchisiz:")))]))
+print(manfiy)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+a=[]
+# for i in range(n):
+#     b = int(input(f"{i+1}-elementni kiriting: "))
+#     a.append(b)
